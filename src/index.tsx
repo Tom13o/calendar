06977c41cc4eed4cb06414e08e8d5296e7e8760a
Tom <90448210+Tom13o@users.noreply.github.com>
2023-04-17 00:03:51 +1000

    yeah i dont even know

--- src/index.tsx
+++ src/index.tsx
@@ -6,22 +6,22 @@ import './index.css'
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
+// import { getFirestore } from 'firebase/firestore';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCmh941_88c15H_21Cmbe0su442x0-7Aec",
-  authDomain: "calendar2076.firebaseapp.com",
-  projectId: "calendar2076",
-  storageBucket: "calendar2076.appspot.com",
-  messagingSenderId: "187067028648",
-  appId: "1:187067028648:web:6bf774a50cbd1db8f5de9d"
+  apiKey: "AIzaSyBzmMKkR4zsGM6bZx1z8vlxwtLXa_XciK0",
+  authDomain: "calendarapp2076.firebaseapp.com",
+  projectId: "calendarapp2076",
+  storageBucket: "calendarapp2076.appspot.com",
+  messagingSenderId: "367370261468",
+  appId: "1:367370261468:web:efd0197251334c1c33b519"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
-export { app, db };
+// const db = getFirestore(app)
+export { app }; // , db };
 
 const container = document.getElementById("root") as HTMLElement;
 const root = ReactDOM.createRoot(container);
